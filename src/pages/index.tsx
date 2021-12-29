8d561184adf3f59f9ff1089924091d7b6b1d0e78
noric <info@noricgepgraphic.com>
2021-12-29 21:48:33 +0900

    TODOリスト作成

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,30 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from "next";
+import Head from "next/head";
+import { useState } from "react";
+
+import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
+  const [todos, setTodos] = useState<string[]>([]);
+
+  const [tmpTodo, setTmpTodo] = useState("");
+
+  const addTodo = () => {
+    if (tmpTodo === "") {
+      alert("文字を入力してください");
+      return;
+    }
+    setTodos([...todos, tmpTodo]);
+    setTmpTodo("");
+  };
+
+  const deleteTodo = (index: number) => {
+    const newTodos = todos.filter((todo, todoIndex) => {
+      return index !== todoIndex;
+    });
+    setTodos(newTodos);
+  };
+
   return (
     <div className={styles.container}>
       <Head>
@@ -13,60 +34,31 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <h1 className={styles.title}>TODOリスト</h1>
+        <div className="form">
+          <input
+            type="text"
+            name="todo"
+            onChange={(e) => setTmpTodo(e.target.value)}
+            value={tmpTodo}
+          />
+          <button onClick={addTodo}>Add</button>
         </div>
+        <ul>
+          {todos.map((todo, index: number) => {
+            return (
+              <li key={index}>
+                {todo}
+                <button onClick={() => deleteTodo(index)}>x</button>
+              </li>
+            );
+          })}
+        </ul>
       </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+      <footer className={styles.footer}></footer>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
